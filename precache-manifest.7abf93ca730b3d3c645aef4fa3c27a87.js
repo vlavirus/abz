1f46f395aaca571311f533d1f505c11d9e7a45fe
Andriy <androando777@gmail.com>
2020-10-20 14:50:45 +0300

    Updates

---
+++ precache-manifest.7abf93ca730b3d3c645aef4fa3c27a87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f96ccebc16f17e9c04e2b8e55ccafa",
+    "url": "/abz/index.html"
+  },
+  {
+    "revision": "82aee3f7a7b4edf5cb96",
+    "url": "/abz/static/css/main.5133f172.chunk.css"
+  },
+  {
+    "revision": "cc20fd45e486d9379282",
+    "url": "/abz/static/js/2.639b0bb7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/abz/static/js/2.639b0bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82aee3f7a7b4edf5cb96",
+    "url": "/abz/static/js/main.6f938a26.chunk.js"
+  },
+  {
+    "revision": "4553421fceaabf9cc091",
+    "url": "/abz/static/js/runtime-main.8ab2d3d1.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
