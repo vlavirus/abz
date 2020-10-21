62e0c8d1d0417b5c650b045b484ee88f44386510
Andriy <androando777@gmail.com>
2020-10-21 15:18:13 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# abz
-* Project: https://vlavirus.github.io/abz/
-* Position: React developer
-* The project goal is to create a game for learning english language
-* Environment: React/Redux/JavaScript/SCSS/HTML
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b99d0ce33a199f1d257c77e686b2ace.js"
+  "/abz/precache-manifest.d2dda13536f19c8fe248a82dafdd2b74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
