6d7a1e5e9478979c6720ac3abf43ae5c785a75f0
Andriy <androando777@gmail.com>
2020-10-21 15:04:23 +0300

    Updates

---
+++ precache-manifest.79ad78254d1462d953cba99300b0e6ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27ffab016a4beb63ebb5e2255ea13cc",
+    "url": "/abz/index.html"
+  },
+  {
+    "revision": "f564432f07d41c55a76b",
+    "url": "/abz/static/css/main.18a8cc2c.chunk.css"
+  },
+  {
+    "revision": "a5aae24560de374ed924",
+    "url": "/abz/static/js/2.fe5e736e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/abz/static/js/2.fe5e736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f564432f07d41c55a76b",
+    "url": "/abz/static/js/main.877e2672.chunk.js"
+  },
+  {
+    "revision": "4553421fceaabf9cc091",
+    "url": "/abz/static/js/runtime-main.8ab2d3d1.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43fbdd20eb51fb664675bf87806c6612.js"
+  "/abz/precache-manifest.79ad78254d1462d953cba99300b0e6ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
