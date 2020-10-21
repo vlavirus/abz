c7831e2315ced13540838e2de313ae29af939947
Andriy <androando777@gmail.com>
2020-10-21 15:01:51 +0300

    Updates

---
+++ precache-manifest.43fbdd20eb51fb664675bf87806c6612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957aa88a32e76720a96f4a155cd11d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89b814ba8d05a4fc819e",
+    "url": "/static/css/main.3c2d38c7.chunk.css"
+  },
+  {
+    "revision": "a5aae24560de374ed924",
+    "url": "/static/js/2.fe5e736e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.fe5e736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b814ba8d05a4fc819e",
+    "url": "/static/js/main.877e2672.chunk.js"
+  },
+  {
+    "revision": "c159dde6961abe44b378",
+    "url": "/static/js/runtime-main.5b622fc1.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abz/precache-manifest.7d6e59ca58725e4c8da8558b23cf527a.js"
+  "/precache-manifest.43fbdd20eb51fb664675bf87806c6612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
